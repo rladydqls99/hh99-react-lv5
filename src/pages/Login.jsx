c5067ce1932ed403a7aee5def68c0284bd7cb54b
rladydqls99 <rladydqls99@naver.com>
2023-09-11 14:00:26 +0900

    페이지 이동 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
   const navigate = useNavigate();
+
+  // id와 pw 상태관리 --------------------------
+  const [userID, setUserID] = useState("");
+  const [userPw, setUserPw] = useState("");
+
+  const getUserId = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setUserID(value);
+  };
+  const getUserPw = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setUserPw(value);
+  };
+  // ----------------------------------------
   return (
     <>
       <h1>로그인 하기</h1>
       <div>
         <h3>아이디</h3>
-        <input type="text" />
+        <input type="text" value={userID} onChange={getUserId} />
         <h3>비밀번호</h3>
-        <input type="password" />
+        <input type="password" value={userPw} onChange={getUserPw} />
       </div>
       <div>
         <button>로그인</button>
